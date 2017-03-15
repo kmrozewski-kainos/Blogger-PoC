45f0cb8614e7c77e87ed135e96912fdbc6f54a8a
Konrad Mrozewski <k.mrozewski@kainos.com>
2017-03-15 22:10:26 +0100

    web frontend services dropwizard, spring (IoC)

---
+++ src/wfeservices/src/main/java/BloggerApplication.java
@@ -0,0 +1,72 @@
+import configuration.BloggerConfiguration;
+import configuration.BloggerSpringConfiguration;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.ws.rs.Path;
+import java.util.EnumSet;
+
+public class BloggerApplication extends Application<BloggerConfiguration> {
+
+	@Override
+	public void run(BloggerConfiguration configuration, Environment environment) throws Exception {
+		AnnotationConfigWebApplicationContext ctx = initializeSpringContext(configuration, environment);
+		enableCors(environment);
+		registerResources(environment, ctx);
+	}
+
+	public static void main(String[] args) {
+		try {
+			new BloggerApplication().run(args);
+		} catch (Exception e) {
+			throw new RuntimeException("Blogger application failed!", e);
+		}
+	}
+
+	private void registerResources(Environment environment, AnnotationConfigWebApplicationContext ctx) {
+		environment.jersey().register(MultiPartFeature.class);
+
+		for (String beanName: ctx.getBeanNamesForAnnotation(Path.class)) {
+			environment.jersey().register(ctx.getBean(beanName));
+		}
+	}
+
+	private void enableCors(Environment environment) {
+		final FilterRegistration.Dynamic cors =
+				environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "*");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+	}
+
+	private AnnotationConfigWebApplicationContext initializeSpringContext(BloggerConfiguration configuration,
+																		  Environment environment) {
+		AnnotationConfigWebApplicationContext parent = new AnnotationConfigWebApplicationContext();
+		parent.refresh();
+		parent.getBeanFactory().registerSingleton("configuration", configuration);
+		parent.getBeanFactory().registerSingleton("dataSource", configuration.getDataSourceFactory().build(environment
+				.metrics(), "dataSource"));
+		parent.registerShutdownHook();
+		parent.start();
+
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.setParent(parent);
+		ctx.register(BloggerSpringConfiguration.class);
+		ctx.refresh();
+		ctx.registerShutdownHook();
+		ctx.start();
+
+		environment.servlets().addServletListeners(new ContextLoaderListener(ctx));
+
+		return ctx;
+	}
+}
---
+++ src/wfeservices/src/main/java/configuration/BloggerSpringConfiguration.java
@@ -0,0 +1,50 @@
+package configuration;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.blogger.poc")
+public class BloggerSpringConfiguration {
+
+	private static final String ENTITIES_PACKAGE = "com.blogger.poc.persistence";
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private BloggerConfiguration bloggerConfiguration;
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setPackagesToScan(ENTITIES_PACKAGE);
+		sessionFactory.setHibernateProperties(hibernateProperties());
+
+		return sessionFactory;
+	}
+
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		return new HibernateTransactionManager(sessionFactory);
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.putAll(bloggerConfiguration.getDataSourceFactory().getProperties());
+
+		return properties;
+	}
+}
